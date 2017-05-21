e29c449348c1b1b34a9b4e27cd2fd3b08539c4c5
gogogohuang <gogogohuang@gmail.com>
2017-05-21 22:15:01 +0800

    native javascript instead of jQuery

--- answers/hw6/js/utils.js
+++ answers/hw6/js/utils.js
@@ -1,43 +1,78 @@
 function getGameStream(game, clientID, limit, offset) {
-    $.ajax({
-        url: "https://api.twitch.tv/kraken/streams",
-        type: "GET",
-        data: { client_id: clientID, game: game, limit: limit, offset: offset },
-        dataType: "json",
-        success: function(data) {
-            removeEmptyChannel();
-            previewPanel(data.streams, offset);
-        },
-        error: function() {
-            console.log("fail");
+    let requestUrl = "https://api.twitch.tv/kraken/streams/?client_id=" + clientID + "&game=" + game + "&limit=" + limit + "&offset=" + offset;
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState == 4) {
+            if (httpRequest.status == 200) {
+                let data = JSON.parse(httpRequest.responseText);
+                removeEmptyChannel();
+                previewPanel(data.streams, offset);
+            }
         }
-    });
+    }
+    httpRequest.open("GET", requestUrl, true);
+    httpRequest.send();
 }
 
 
-function previewPanel(data, offset) {
+function previewPanel(streamData, offset) {
     let previewContent = [];
 
-    if (typeof data !== 'undefined') {
-        data.forEach((ele, idx) => {
-            idx = idx + offset;
-            $('#row').append($('<div>', { 'class': 'chanel' }));
-            $('.chanel').eq(idx).append($('<div>', { 'class': 'preview' }).append($('<img>', { 'src': ele.preview.large })));
-            $('.chanel').eq(idx).append($('<div>', { 'class': 'info' }).append($('<div>', { 'class': 'avatar' }).append($('<img>', { 'src': ele.channel.logo }))));
-            $('.chanel .info').eq(idx).append($('<div>', { 'class': 'intro' }).append($('<div>', { 'class': 'name', 'text': ele.channel.status })));
-            $('.chanel .info .intro').eq(idx).append($('<div>', { 'class': 'onwer', 'text': ele.channel.display_name }));
+    if (typeof streamData !== 'undefined') {
+        streamData.forEach((ele, idx) => {
+            let chanelDiv = document.createElement('div');
+            chanelDiv.className = 'chanel';
+            let previewDiv = document.createElement('div');
+            previewDiv.className = 'preview';
+
+            let previewImg = document.createElement('img');
+            previewImg.src = ele.preview.large;
+            previewDiv.appendChild(previewImg);
+            chanelDiv.appendChild(previewDiv);
+
+            let infoDiv = document.createElement('div');
+            infoDiv.className = "info";
+            let avatarDiv = document.createElement('div');
+            avatarDiv.className = "avatar";
+            let avatarImg = document.createElement('img');
+            avatarImg.src = ele.channel.logo;
+            avatarDiv.appendChild(avatarImg);
+            infoDiv.appendChild(avatarDiv);
+            chanelDiv.appendChild(infoDiv);
+
+            let introDiv = document.createElement('div');
+            introDiv.className = "intro";
+            let nameDiv = document.createElement('div');
+            nameDiv.className = "name";
+            nameDiv.innerText = ele.channel.status;
+            introDiv.appendChild(nameDiv);
+            let onwerDiv = document.createElement('div');
+            onwerDiv.classNam = "onwer";
+            onwerDiv.innerText = ele.channel.display_name;
+            introDiv.appendChild(onwerDiv);
+            chanelDiv.appendChild(introDiv);
+            infoDiv.appendChild(introDiv);
+            document.getElementById("row").appendChild(chanelDiv);
+
         });
         addEmptyChannel();
     }
 }
 
 function addEmptyChannel() {
-    let emptyChannel = '<div class="chanel"></div><div class="chanel">';
-    $('#row').append(emptyChannel);
+    for (let i = 0; i < 2; i++) {
+        let emptyChannel = document.createElement('div');
+        emptyChannel.className = 'chanel';
+        emptyChannel.id = "emptyChannel";
+        document.getElementById("row").appendChild(emptyChannel);
+    }
 }
 
 function removeEmptyChannel() {
-    $('.chanel').slice(-2).remove();
+    let emptyChannel = document.querySelectorAll('#emptyChannel');
+    emptyChannel.forEach((ele, idx) => {
+        ele.remove();
+    });
 }
 
 function debounce(fn, delay) {
@@ -56,8 +91,8 @@ function init() {
     let page = 0;
     getGameStream('League of Legends', 'pv5emcr029nlrokiw16n0uh5awus0h', 20, 0);
 
-    $(window).on('scroll', debounce(function(event) {
-        if ($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+    document.addEventListener('scroll', debounce(function(event) {
+        if (document.body.scrollTop + window.innerHeight > document.body.scrollHeight - 100) {
             page++;
             getGameStream('League of Legends', 'pv5emcr029nlrokiw16n0uh5awus0h', 20, 20 * (page));
         }
