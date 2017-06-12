4b5db2a266a47dc9ab3ab657c595b7b94a9b644e
gogogohuang <gogogohuang@gmail.com>
2017-06-12 23:41:11 +0800

    inline

--- answers/hw9/dist/bundle.js
+++ answers/hw9/dist/bundle.js
@@ -63,138 +63,11 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 5);
+/******/ 	return __webpack_require__(__webpack_require__.s = 4);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__i18n_language__ = __webpack_require__(3);
- 
-
- let $ = __webpack_require__(1);
-
- const stream = {
-     category: 'League of Legends',
-     clientId: 'pv5emcr029nlrokiw16n0uh5awus0h',
- };
- let page = 0;
- let lang = 'zh-tw';
-
- function getGameStream(game, clientID, limit, offset, language) {
-     $.ajax({
-         url: "https://api.twitch.tv/kraken/streams",
-         type: "GET",
-         data: { client_id: clientID, game: game, limit: limit, offset: offset, language: language },
-         dataType: "json",
-         success: function(data) {
-             removeEmptyChannel();
-             previewPanel(data.streams, offset);
-         },
-         error: function() {
-             console.log("fail");
-         }
-     });
- }
-
- function previewPanel(data, offset) {
-     let previewContent;
-
-     if (typeof data !== 'undefined') {
-         data.forEach((ele, idx) => {
-             idx = idx + offset;
-             $('#row').append($('<div>', { 'class': 'chanel' }));
-             $('.chanel').eq(idx).append($('<div>', { 'class': 'preview' }).append($('<img>', { 'src': ele.preview.large })));
-             $('.chanel').eq(idx).append($('<div>', { 'class': 'info' }).append($('<div>', { 'class': 'avatar' }).append($('<img>', { 'src': ele.channel.logo }))));
-             $('.chanel .info').eq(idx).append($('<div>', { 'class': 'intro' }).append($('<div>', { 'class': 'name', 'text': ele.channel.status })));
-             $('.chanel .info .intro').eq(idx).append($('<div>', { 'class': 'onwer', 'text': ele.channel.display_name }));
-         });
-         addEmptyChannel();
-     }
- }
-
- function addEmptyChannel() {
-     let emptyChannel = '<div class="chanel id="emptyChannel"></div><div class="chanel" id="emptyChannel">';
-     $('#row').append(emptyChannel);
- }
-
- function removeEmptyChannel() {
-     $('.chanel').slice(-2).remove();
- }
-
- function debounce(fn, delay) {
-     let timer = null;
-     return function() {
-         let context = this,
-             args = arguments;
-         clearTimeout(timer);
-         timer = setTimeout(function() {
-             fn.apply(context, args);
-         }, delay);
-     };
- }
-
- function titleLayout(lang) {
-     $("#title").animate({
-         opacity: 0,
-     }, 200, "linear", function() {
-         $('#title').text("");
-     });
-
-     $("#title").animate({
-         opacity: 1,
-     }, 100, "linear", function() {
-         $('#title').text(__WEBPACK_IMPORTED_MODULE_0__i18n_language__["a" /* default */][lang].title);
-     });
-
-     $('button').removeClass('active');
-     $('#' + lang).addClass('active');
-     $(".row").empty();
-
-     getGameStream(stream.category, stream.clientId, 20, 0, lang);
- }
-
- function changeLan(selectedLang) {
-     if (selectedLang === lang) {
-         return;
-     }
-
-     lang = selectedLang;
-     page = 0;
-     titleLayout(lang);
- }
-
-
-
- function init() {
-
-     $('#en').on('click', (e) => {
-         changeLan('en');
-     });
-
-     $('#zh-tw').on('click', (e) => {
-         changeLan('zh-tw');
-     });
-
-     titleLayout(lang)
-     $(window).on('scroll', debounce(function(event) {
-         if ($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
-             page++;
-             getGameStream(stream.category, stream.clientId, 20, 20 * (page), lang);
-         }
-     }, 250));
- }
-
-
- $(document).ready(() => {
-     init();
- });
-
-/***/ }),
-/* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
 var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*!
@@ -10454,6 +10327,21 @@ return jQuery;
 
 
 /***/ }),
+/* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__en__ = __webpack_require__(2);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__zh_tw__ = __webpack_require__(3);
+
+
+
+/* harmony default export */ __webpack_exports__["a"] = ({
+    "en": __WEBPACK_IMPORTED_MODULE_0__en__["a" /* default */],
+    "zh-tw": __WEBPACK_IMPORTED_MODULE_1__zh_tw__["a" /* default */],
+});
+
+/***/ }),
 /* 2 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
@@ -10467,14 +10355,8 @@ return jQuery;
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__en__ = __webpack_require__(2);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__zh_tw__ = __webpack_require__(4);
-
-
-
 /* harmony default export */ __webpack_exports__["a"] = ({
-    "en": __WEBPACK_IMPORTED_MODULE_0__en__["a" /* default */],
-    "zh-tw": __WEBPACK_IMPORTED_MODULE_1__zh_tw__["a" /* default */],
+    'title': '用中文直播的頻道'
 });
 
 /***/ }),
@@ -10482,17 +10364,127 @@ return jQuery;
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony default export */ __webpack_exports__["a"] = ({
-    'title': '用中文直播的頻道'
-});
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__i18n_language__ = __webpack_require__(1);
+ 
 
-/***/ }),
-/* 5 */
-/***/ (function(module, exports, __webpack_require__) {
+ let $ = __webpack_require__(0);
 
-__webpack_require__(0);
-(function webpackMissingModule() { throw new Error("Cannot find module \"watch\""); }());
+ const stream = {
+     category: 'League of Legends',
+     clientId: 'pv5emcr029nlrokiw16n0uh5awus0h',
+ };
+ let page = 0;
+ let lang = 'zh-tw';
 
+ function getGameStream(game, clientID, limit, offset, language) {
+     $.ajax({
+         url: "https://api.twitch.tv/kraken/streams",
+         type: "GET",
+         data: { client_id: clientID, game: game, limit: limit, offset: offset, language: language },
+         dataType: "json",
+         success: function(data) {
+             removeEmptyChannel();
+             previewPanel(data.streams, offset);
+         },
+         error: function() {
+             console.log("fail");
+         }
+     });
+ }
+
+ function previewPanel(data, offset) {
+     let previewContent;
+
+     if (typeof data !== 'undefined') {
+         data.forEach((ele, idx) => {
+             idx = idx + offset;
+             $('#row').append($('<div>', { 'class': 'chanel' }));
+             $('.chanel').eq(idx).append($('<div>', { 'class': 'preview' }).append($('<img>', { 'src': ele.preview.large })));
+             $('.chanel').eq(idx).append($('<div>', { 'class': 'info' }).append($('<div>', { 'class': 'avatar' }).append($('<img>', { 'src': ele.channel.logo }))));
+             $('.chanel .info').eq(idx).append($('<div>', { 'class': 'intro' }).append($('<div>', { 'class': 'name', 'text': ele.channel.status })));
+             $('.chanel .info .intro').eq(idx).append($('<div>', { 'class': 'onwer', 'text': ele.channel.display_name }));
+         });
+         addEmptyChannel();
+     }
+ }
+
+ function addEmptyChannel() {
+     let emptyChannel = '<div class="chanel id="emptyChannel"></div><div class="chanel" id="emptyChannel">';
+     $('#row').append(emptyChannel);
+ }
+
+ function removeEmptyChannel() {
+     $('.chanel').slice(-2).remove();
+ }
+
+ function debounce(fn, delay) {
+     let timer = null;
+     return function() {
+         let context = this,
+             args = arguments;
+         clearTimeout(timer);
+         timer = setTimeout(function() {
+             fn.apply(context, args);
+         }, delay);
+     };
+ }
+
+ function titleLayout(lang) {
+     $("#title").animate({
+         opacity: 0,
+     }, 200, "linear", function() {
+         $('#title').text("");
+     });
+
+     $("#title").animate({
+         opacity: 1,
+     }, 100, "linear", function() {
+         $('#title').text(__WEBPACK_IMPORTED_MODULE_0__i18n_language__["a" /* default */][lang].title);
+     });
+
+     $('button').removeClass('active');
+     $('#' + lang).addClass('active');
+     $(".row").empty();
+
+     getGameStream(stream.category, stream.clientId, 20, 0, lang);
+ }
+
+ function changeLan(selectedLang) {
+     if (selectedLang === lang) {
+         return;
+     }
+
+     lang = selectedLang;
+     page = 0;
+     titleLayout(lang);
+ }
+
+
+
+ function init() {
+
+     $('#en').on('click', (e) => {
+         changeLan('en');
+     });
+
+     $('#zh-tw').on('click', (e) => {
+         changeLan('zh-tw');
+     });
+
+     titleLayout(lang)
+     $(window).on('scroll', debounce(function(event) {
+         if ($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+             page++;
+             getGameStream(stream.category, stream.clientId, 20, 20 * (page), lang);
+         }
+     }, 250));
+ }
+
+
+ $(document).ready(() => {
+     init();
+ });
 
 /***/ })
 /******/ ]);
--- answers/hw9/gulpfile.js
+++ answers/hw9/gulpfile.js
@@ -1,52 +1,29 @@
-// 這邊就是引入各種 gulp 的 plugin，用法可以自己用關鍵字去查
 var gulp = require('gulp');
-var babel = require('gulp-babel');
-var rename = require("gulp-rename");
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
 var gulpSequence = require('gulp-sequence');
-var cleanCSS = require('gulp-clean-css');
+var inline = require('gulp-inline');
 var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var babel = require('gulp-babel');
 
-// 這個是把 cool.scss compile 成 css，然後寫到 dist 這個資料夾底下
-gulp.task('sass', function() {
-    return gulp.src('sass/style.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('dist'));
-});
-
-// 這個是把 my-es6.js 用 babel compile，重新命名成 my.js 然後存到 dist 底下
-gulp.task('babel', function() {
-    return gulp.src('src/js/*.js')
-        .pipe(babel())
-        .pipe(rename('index.js'))
-        .pipe(gulp.dest('dist'));
-})
-
-// 這個是壓縮 CSS，並且重新命名叫 cool.min.css
-gulp.task('minify-css', function() {
-    return gulp.src('stylesheets/*.css')
-        .pipe(cleanCSS())
-        .pipe(rename('style.min.css'))
-        .pipe(gulp.dest('dist'));
-})
-
-// 這個是壓縮 js，重新命名成 my.min.js
-// gulp.task('uglify-js', function() {
-//     return gulp.src('dist/index.js')
-//         .pipe(uglify())
-//         .pipe(rename('index.min.js'))
-//         .pipe(gulp.dest('dist'));
-// })
-
-gulp.task('scripts', function() {
-    return gulp.src(['src/js/*.js', 'src/js/i18n/*.js'])
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('dist'))
-        .pipe(rename('all.min.js'))
+gulp.task('uglify:js', function() {
+    gulp.src('dist/*.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(rename('bundle.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('dist/'))
 });
 
-// 這個是為了讓上面的任務有順序的跑
-gulp.task('default', gulpSequence('sass', 'minify-css', 'scripts'));
+gulp.task('inline', function() {
+    return gulp.src('index.html')
+        .pipe(inline({
+            base: '/',
+            //js: uglify,
+            css: minifyCss,
+        }))
+        .pipe(gulp.dest('dist/'));
+})
+
+gulp.task('default', gulpSequence('uglify:js', 'inline'));
